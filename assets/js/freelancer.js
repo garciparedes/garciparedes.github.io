55baea05bf7aabed2fe7514da06f02f6bbc14372
Sergio Garcia Prado <sergio@garciparedes.me>
2018-04-30 13:42:14 +0200

    Minor improvements

--- assets/js/freelancer.js
+++ assets/js/freelancer.js
@@ -9,7 +9,7 @@ $(function() {
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - $('#main-navbar').height()
+            scrollTop: $($anchor.attr('href')).offset().top
         }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
